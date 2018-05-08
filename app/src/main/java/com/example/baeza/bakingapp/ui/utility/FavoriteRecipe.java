530319b1aca9380b7a3c2a31e4bf9e54994169c3
Singorenko <cbaezenko@gmail.com>
2018-05-08 18:18:52 +0300

    add recipe ingredient list is shown in the widget after the retrofit answer. Problem: when the app opens and the answer still don't arrive the app crush.
    add structure for a preferences value.

---
+++ app/src/main/java/com/example/baeza/bakingapp/ui/utility/FavoriteRecipe.java
@@ -0,0 +1,18 @@
+package com.example.baeza.bakingapp.ui.utility;
+
+import android.content.Context;
+
+public class FavoriteRecipe {
+
+    private Context context;
+
+    public FavoriteRecipe(){}
+
+    public FavoriteRecipe(Context context){
+        this.context = context;
+    }
+
+    public void saveToPref(){}
+
+    public void getFromPref(){}
+}
